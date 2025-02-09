8dbae07c0c26646e2b074baca86362932713e279
bebop <mrbebap@gmail.com>
2025-02-08 19:26:57 -0600

    Add files via upload

---
+++ update.ps1
@@ -0,0 +1,41 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+function Update-Windows {
+    Write-Host "Installing Dependencies..." -ForegroundColor Cyan
+
+    Write-Host "Installing NuGet..." -ForegroundColor Cyan
+    if (-not (Get-Module -ListAvailable -Name NuGet)) {
+        Install-PackageProvider -Name NuGet -ForceBootstrap -Force
+        Write-Host "Successfully Installed NuGet! Continuing Updates..." -ForegroundColor Magenta
+    }
+
+    else {
+        Write-Host "NuGet is already installed. Continuing Updates..." -ForegroundColor Magenta
+    }
+
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+    Write-Host "Checking for Windows Updates..." -ForegroundColor Cyan
+    if (-not (Get-Module -ListAvailable -Name PSWindowsUpdate )) {
+        Install-Module -Name PSWindowsUpdate -Force -Confirm:$false -SkipPublisherCheck
+    }
+    
+    Get-WindowsUpdate -Install -AcceptAll -AutoReboot
+    Write-Host "Windows Updates completed!" -ForegroundColor Cyan
+
+
+}
+
+function Update-MicrosoftStore {
+    Write-Host "Checking for MS Store Updates..." -ForegroundColor Cyan
+    winget upgrade --all --accept-source-agreements --accept-package-agreements
+} 
+
+
+Update-Windows
+Update-MicrosoftStore
+
+Set-ExecutionPolicy Restricted -Confirm:$false -Force
+
+Write-Host "Remember to restart device to complete updates!" -ForegroundColor Magenta
+Write-Host ""
+Write-Host "Finished Updates!" -ForegroundColor Green
